91e3257e79b2f39b4c8633be5a4ce60ae75ae202
schoening <oliverschoning@gmail.com>
2017-02-24 13:45:32 +0100

    Major re-write of the code

--- pathfinder.cr
+++ pathfinder.cr
@@ -1,162 +1,93 @@
-class Pathfinder
-  property allow_diagonals : Bool, graph : Pathfinder::Graph
+class Pathfinder2
+  alias Node = NamedTuple(x: Int32, y: Int32)
+  property grid : Array(Array(Int32))
 
-  def initialize(grid : Array(Array(Int32)), allow_diagonals = false)
-    @graph = Graph.new grid
-    @start = {"x" => 0, "y" => 0}
-    @dest = {"x" => 0, "y" => 0}
-    @allow_diagonals = allow_diagonals
+  def initialize(grid : Array(Array(Int32)))
+    @grid = grid.clone
+
+    @start = Point.new 0, 0
+    @dest = Point.new 0, 0
   end
 
-  def start(x, y)
-    @start["x"] = x
-    @start["y"] = y
+  def start(x : Int32, y : Int32)
+    @start.x = x
+    @start.y = y
   end
 
-  def dest(x, y)
-    @dest["x"] = x
-    @dest["y"] = y
+  def dest(x : Int32, y : Int32)
+    @dest.x = x
+    @dest.y = y
   end
 
-  protected def generate_path(visited)
-    path = Array(Tuple(Int32, Int32)).new
-    node = visited.last
-    while true
-      parent = node.parent
-      if parent.nil?
+  def search
+    queue = Deque(Node).new
+    visited = Deque(Node).new
+    parents = Hash(Node, Node).new
+
+    start = {x: @start.x, y: @start.y}
+
+    queue << start
+
+    current = start
+
+    path_found = false
+    while queue.size > 0
+      current = queue.shift
+      visited << current
+
+      if current[:x] == @dest.x && current[:y] == @dest.y
+        path_found = true
         break
+      end
+
+      nb = neighbours(current)
+
+      nb.each do |x, y|
+        node = {x: x, y: y}
+        if !visited.includes?(node) && !queue.includes?(node)
+          queue << node
+          parents[node] = current
+        end
+      end
+    end
+
+    path = Array(Node).new
+    if path_found == false
+      return path
+    end
+
+    current = visited.pop
+    searching = true
+    while searching
+      if parents.has_key? current
+        path << current
+        current = parents[current]
       else
-        path << {node.x, node.y}
-        node = parent
+        searching = false
       end
     end
     path.reverse
   end
 
-  # TODO:
-  # set all parents to nil to avoid potential bugs after more than one search?
-  # Still need to verify if this is ever a problem.
-  def search
-    # Nodes that are in queue to be checked
-    todo = Array(Node).new
-    visited = Array(Node).new
-    #
-    node_grid = @graph.grid
-    #
-    start_node = node_grid[@start["x"]][@start["y"]]
-    dest_node = node_grid[@dest["x"]][@dest["y"]]
-    #
-    todo << start_node
-    path = Array(Tuple(Int32, Int32)).new
-    while todo.size > 0
-      current_node = todo.shift
-      visited << current_node
-
-      # Destination found!
-      if current_node == dest_node
-        return generate_path visited
-      end
-
-      x = current_node.x
-      y = current_node.y
-      # UP
-      if within_boundary_of? x, y - 1, node_grid
-        node = node_grid[x][y - 1]
-        if node.weight != 0 && !node.inside?(visited) && !node.inside?(todo)
-          todo << node
-          node.parent = current_node
-        end
-      end
-      # DOWN
-      if within_boundary_of? x, y + 1, node_grid
-        node = node_grid[x][y + 1]
-        if node.weight != 0 && !node.inside?(visited) && !node.inside?(todo)
-          todo << node
-          node.parent = current_node
-        end
-      end
-      # LEFT
-      if within_boundary_of? x - 1, y, node_grid
-        node = node_grid[x - 1][y]
-        if node.weight != 0 && !node.inside?(visited) && !node.inside?(todo)
-          todo << node
-          node.parent = current_node
-        end
-      end
-      # RIGHT
-      if within_boundary_of? x + 1, y, node_grid
-        node = node_grid[x + 1][y]
-        if node.weight != 0 && !node.inside?(visited) && !node.inside?(todo)
-          todo << node
-          node.parent = current_node
-        end
-      end
-      # TODO: Prevent diagonals at the edges of walls.
-      if @allow_diagonals == true
-        # NORTH WEST
-        if within_boundary_of? x - 1, y - 1, node_grid
-          node = node_grid[x - 1][y - 1]
-          if node.weight != 0 && !node.inside?(visited) && !node.inside?(todo)
-            todo << node
-            node.parent = current_node
-          end
-        end
-        # NORTH EAST
-        if within_boundary_of? x + 1, y - 1, node_grid
-          node = node_grid[x + 1][y - 1]
-          if node.weight != 0 && !node.inside?(visited) && !node.inside?(todo)
-            todo << node
-            node.parent = current_node
-          end
-        end
-        # SOUTH EAST
-        if within_boundary_of? x + 1, y + 1, node_grid
-          node = node_grid[x + 1][y + 1]
-          if node.weight != 0 && !node.inside?(visited) && !node.inside?(todo)
-            todo << node
-            node.parent = current_node
-          end
-        end
-        # SOUTH WEST
-        if within_boundary_of? x - 1, y + 1, node_grid
-          node = node_grid[x - 1][y + 1]
-          if node.weight != 0 && !node.inside?(visited) && !node.inside?(todo)
-            todo << node
-            node.parent = current_node
-          end
-        end
-      end
+  # Returns a list of positions that surround the current node,
+  # are empty (value = 1),
+  # and within the bounds of the @grid.
+  def neighbours(node)
+    x = node[:x]
+    y = node[:y]
+    [{x, y - 1}, {x, y + 1}, {x - 1, y}, {x + 1, y}].select do |x1, y1|
+      x1 >= 0 && x1 < @grid.size && y1 >= 0 && y1 < @grid[0].size && @grid[x1][y1] == 1
     end
-    # Returns empty path array
-    path
   end
 
   protected def within_boundary_of?(x, y, grid)
     x >= 0 && x < grid.size && y >= 0 && y < grid[0].size
   end
 
-  class Graph
-    property grid : Array(Array(Node))
+  class Point
+    property x, y
 
-    def initialize(matrix : Array(Array(Int32)))
-      @grid = Array(Array(Node)).new
-      matrix.each_with_index do |row, x|
-        @grid << Array(Node).new
-        row.each_with_index do |col, y|
-          @grid[x] << Node.new x, y, nil, col
-        end
-      end
-    end
-  end
-
-  class Node
-    property x, y, parent, weight
-
-    def initialize(@x : Int32, @y : Int32, @parent : Node | Nil, @weight : Int32)
-    end
-
-    def inside?(array)
-      array.select { |i| i == self }.size > 0
+    def initialize(@x : Int32, @y : Int32)
     end
   end
 end
